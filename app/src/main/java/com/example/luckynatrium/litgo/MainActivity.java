4339b30b3d71d5e35884575a8aa2e305cd8650de
robertovna <g_arshakyan@list.ru>
2019-03-13 19:51:01 +0300

    Добавлено боковое меню

--- app/src/main/java/com/example/luckynatrium/litgo/MainActivity.java
+++ app/src/main/java/com/example/luckynatrium/litgo/MainActivity.java
@@ -1,29 +1,45 @@
 package com.example.luckynatrium.litgo;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+    private TableLayout tl;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView_hw = (TextView)findViewById(R.id.main_hw);
-        textView_hw.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                OpenQuizActivity();
-            }
-        });
+
+        drawerLayout =(DrawerLayout)findViewById(R.id.activity_main);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
     private void OpenQuizActivity() {
         startActivity(new Intent(this, QuizActivity.class));
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if (drawerToggle.onOptionsItemSelected(item))
+            return true;
+        return super.onOptionsItemSelected(item);
+    }
+
 }
